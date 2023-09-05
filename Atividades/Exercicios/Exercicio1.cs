4eb97be7a325c1660f2d1f9658e963502ca70189
Julio Cesar Nardi <julio.cesarnardi88@gmail.com>
2023-09-05 16:10:41 -0300

    Segundo Commit

---
+++ Atividades/Exercicios/Exercicio1.cs
@@ -0,0 +1,28 @@
+﻿int[] numbers = new int[10];
+Random rnd = new Random();
+
+// preenche o vetor
+
+for (int i = 0; i < numbers.Length; i++) 
+{
+    numbers[i] = rnd.Next(1, 99);
+}
+
+// Imprime vetor desordenado
+
+Console.WriteLine("Lista original:");
+for (int i = 0; i < numbers.Length; i++) 
+{
+    Console.WriteLine(numbers[i]);
+}
+
+
+// Oderna valores
+Array.Sort(numbers);
+
+Console.WriteLine("Lista ordenada:");
+// Imprime valor ordenado
+for (int i = 0; i < numbers.Length; i++) 
+{
+    Console.WriteLine(numbers[i]);
+}
