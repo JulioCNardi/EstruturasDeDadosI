dc391018a83e54f1184890ac9cdcace92adac871
Julio Cesar Nardi <407472@unoesc.lan>
2023-09-06 22:19:33 -0300

    Aula Quatro

---
+++ Atividades/Pilha/Program.cs
@@ -0,0 +1,16 @@
+using StackClass;
+
+Stack pilha = new Stack(); // Stack é o metodo construtor
+
+pilha.Push(10);
+pilha.Push(9);
+pilha.Push(100);
+pilha.Push(50);
+
+pilha.Pick();
+
+
+pilha.PrintStack();
+
+
+
