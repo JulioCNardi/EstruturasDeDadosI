c98aa681d5403c44b1801451e419bc83c7c84af7
Julio Cesar Nardi <julio.cesarnardi88@gmail.com>
2023-09-11 21:09:41 -0300

    Pilha String

---
+++ Atividades/PilhaString/Program.cs
@@ -0,0 +1,13 @@
+﻿using StackClass;
+
+StackStr pilha = new StackStr(); // Stack é o metodo construtor
+
+pilha.Push("10");
+pilha.Pop("10");
+pilha.Push("100");
+pilha.Push("50");
+
+pilha.Pick();
+
+
+pilha.PrintStack();
---
+++ Atividades/PilhaString/StrackStr.cs
@@ -0,0 +1,74 @@
+using System;
+
+namespace StackClass 
+{
+     public class StackStr
+    {
+        static readonly int MAX = 10;
+        int top = -1;
+        string [] stack = new string[MAX];
+
+        public bool IsEmpty() 
+        {
+            return (top < 0);
+        }
+        public bool Push(string data)
+        {   
+            if (top >= MAX) 
+            {
+                Console.WriteLine("Stack Overflow");
+                return false;
+            }
+
+            top += 1;
+            stack[top] = data;
+
+            return true;
+        }
+
+        public int Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return 0;
+            }
+
+            int valor = Int32.Parse(stack[top]);
+            top -= 1;
+            return valor;
+
+        }
+
+        public void Pick() 
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+
+            Console.WriteLine($"O topo da pilha é: {stack[top]}");
+        }
+
+                public void PrintStack()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+            
+            Console.WriteLine("Os itens da pilha são:");
+
+            for (int i = top; i >= 0; i--)
+            {
+                string texto = $"Stack[{stack[i]}] ";
+                Console.WriteLine(texto);
+            }
+        }
+
+
+
+    }
+}
